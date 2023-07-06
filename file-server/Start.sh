bccf842f4244dc8d737a1d8f9dc7e9dcb41be85a
xinnj <xinnj@hotmail.com>
2023-07-06 18:47:37 +0800

    Support URL prefix

---
+++ file-server/Start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+cp -rf /source/fancyindex /data/
+mkdir -p /data${URL_PREFIX}
+cp -rf /source/ios-install-images /data${URL_PREFIX}/
+chown uploader:uploader /data${URL_PREFIX}
+chown -R uploader:uploader /data${URL_PREFIX}/ios-install-images
+sed -i -e "s#<URL_PREFIX>#${URL_PREFIX}#g" /etc/nginx/nginx.conf
+/usr/sbin/nginx -g 'daemon off;'
